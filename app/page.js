6a405a81a3541a0d4f0aaa6615c8f6f4bc9cfcc1
Paritosh Chaudhari <chaudhariparitosh9571@gmail.com>
2023-09-16 10:06:34 +0530

    Added FAQ Section

--- app/page.js
+++ app/page.js
@@ -5,6 +5,8 @@ import Map from '@/Components/Body/Map';
 import Table from '@/Components/Table/Table';
 import DetailsInfoCard from '@/Components/Body/DetailInfoCard';
 import Footer from '@/Components/Footer/Footer';
+import FAQ from '@/Components/FAQ/FAQ'
+// import Accordion from '@/Components/Accordion/app';
 
 
 
@@ -15,6 +17,7 @@ function App() {
     <Map/>
     <Table/>
     <DetailsInfoCard/>
+    <FAQ/>
     <Footer/>
     </>
    
